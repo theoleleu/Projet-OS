85efea6d471bd69cb168ab86a2e7b258e6f32a8a
Théo L <theo.leleu3@etu.univ-lorraine.fr>
2019-11-19 10:48:26 +0100

    Initial commit

---
+++ f1.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //printf
+#include "f.h"
+int funf2();
+int main(){
+    printf("Je suis f1.c !\n");
+    funf2();
+    return 0;
+}
---
+++ f2.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //printf
+
+int funf2(){
+    printf("Je suis f2.c ! ahah !\n");
+    return 0;
+}
